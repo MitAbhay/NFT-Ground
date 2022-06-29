0a1c42bf9c392faf7bb4af29bda2fd01e18a0f65
Abhay <leader.abhay002@gmail.com>
2022-06-29 20:46:24 +0530

    ThirdWeb applied

--- pages/nft/[id].tsx
+++ pages/nft/[id].tsx
@@ -1,6 +1,11 @@
 import React from "react";
+import { useAddress, useDisconnect, useMetamask } from "@thirdweb-dev/react";
 
 function NFTDrop() {
+  const connectWithMetamask = useMetamask();
+  const address = useAddress();
+  const disconnect = useDisconnect();
+  console.log(address);
   return (
     <div className="flex flex-col h-screen lg:grid lg:grid-cols-10">
       {/* LEFT */}
@@ -22,9 +27,9 @@ function NFTDrop() {
         </div>
       </div>
       {/* RIGHT */}
-      <div className="flex flex-col lg:col-span-6 justify-between">
+      <div className="flex flex-col lg:col-span-6 justify-between text-center">
         {/* HEADER */}
-        <header className="flex items-center justify-between mx-8 my-4">
+        <header className="flex items-center justify-between mx-8 mt-4">
           <h1 className="text-gray-500 w-52 sm:w-72">
             The{" "}
             <span className="font-extrabold underline decoration-pink-600/50">
@@ -32,10 +37,20 @@ function NFTDrop() {
             </span>
             for NFT Marketplace
           </h1>
-          <button className="hover:bg-yellow-600 rounded-full bg-yellow-500 px-4 py-1 cursor-pointer text-white text-xs lg:text-sm">
-            Sign In
+          <button
+            onClick={address ? disconnect : connectWithMetamask}
+            className="hover:bg-yellow-600 rounded-full bg-yellow-500 px-4 py-1 cursor-pointer text-white text-xs lg:text-sm"
+          >
+            {address ? "Sign Out" : "Sign In "}
           </button>
         </header>
+
+        {address && (
+          <p className = "text-green-600">
+            You are logged in with Wallet <span className="font-bold">{address.substring(0, 5)}...
+            {address.substring(address.length - 5)}</span>
+          </p>
+        )}
         {/* content */}
         <div className="m-4 items-center flex flex-col mt-10">
           <img
@@ -46,9 +61,11 @@ function NFTDrop() {
           <h1 className="my-4 text-3xl font-bold lg:text-5xl lg:font-extrabold">
             The NFT Ground Club | NFT Drop
           </h1>
-          <p className = "text-green-500 p-2">13 / 25 NFT Claimed</p>
+          <p className="text-green-500 p-2">13 / 25 NFT Claimed</p>
         </div>
-        <button className="bg-pink-800 px-4 py-2 mx-2 mb-4 rounded-full text-white text-lg">Mint NFT (0.02 ETH)</button>
+        <button className="bg-pink-800 px-4 py-2 mx-2 mb-4 rounded-full text-white text-lg">
+          Mint NFT (0.02 ETH)
+        </button>
       </div>
     </div>
   );
