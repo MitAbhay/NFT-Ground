b58380f2e4825d46c139ab5e04104334fee389f4
Abhay <leader.abhay002@gmail.com>
2022-07-03 10:30:02 +0530

    added next-sanity

---
+++ sanity.js
@@ -0,0 +1,11 @@
+import { createCurrentUserHook, createClient } from "next-sanity";
+
+import { createImageUrlBuilder } from "@sanity/image-url";
+
+export const config = {
+    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
+    apiVersion: "2022-07-03",
+    useCdn: process.env.NODE_ENV === "production",
+    token: process.env.SANITY_TOKEN,
+}
