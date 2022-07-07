c291cf28dc79474198563dfd082dbcd5ad1a032c
Abhay <leader.abhay002@gmail.com>
2022-07-07 20:59:08 +0530

    style home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,25 +11,27 @@ interface Props {
 
 const Home: NextPage<Props> = ({ collections }) => {
   return (
-    <div className="flex min-h-screen flex-col max-w-7xl mx-auto items-center justify-center py-2">
+    <div className="flex min-h-screen flex-col max-w-7xl mx-auto items-center justify-center py-2 bg-gray-200">
       <Head>
         <title>NFT Ground</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1 className="">NFT GROUND FOR ALL</h1>
+      <h1 className="text-6xl font-bold text-red-500 p-2">
+        NFT GROUND for Beginners
+      </h1>
       <main className="p-10 shadow-xl">
         <div className="grid space-x-3 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
           {collections.map((collection) => (
-            <Link href={`/nft/${collection.slug.current}`}>
+            <Link key = {collection.title} href={`/nft/${collection.slug.current}`}>
               <div className="flex flex-col items-center cursor-pointer transition-all duration-200 hover:scale-105">
                 <img
                   className="h-94 w-60 object-cover rounded-2xl"
                   src={urlFor(collection.previewImage).url()}
                   alt=""
                 />
-                <div>
+                <div className="mt-2">
                   <h1 className="text-3xl">{collection.title}</h1>
-                  <p className="text-sm text-gray-300 mt-2">
+                  <p className="text-sm text-gray-500 mt-2">
                     {collection.description}
                   </p>
                 </div>
